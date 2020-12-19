b389f2ba28e0465491847d446d1a6d0eed4bc267
Subhayu Kumar Bala <balasubhayu99@gmail.com>
2020-12-20 01:31:16 +0530

    Update todo.py

--- todo.py
+++ todo.py
@@ -11,16 +11,16 @@ def todo(arg):
 	if (arg[2]  == "help" or arg[2]  == ""):
 		print('Usage :-\n$ ./todo add "todo item"  # Add a new todo\n$ ./todo ls               # Show remaining todos\n$ ./todo del NUMBER       # Delete a todo\n$ ./todo done NUMBER      # Complete a todo\n$ ./todo help             # Show usage\n$ ./todo report           # Statistics')
 			
-	else if (arg[2]  == "ls"):
+	elif (arg[2]  == "ls"):
 		for task, index in reversed(list(enumerate(todoarray))):
 			print("[{}] {}".format(index+1, task))
 		
-	else if (arg[2] == "add"):
+	elif (arg[2] == "add"):
 		if(arg[3]):
 			todoarray.append(arg[3])
 			print("Added todo: {}".format(arg[3]))
 		
-	else if(arg[2] == "del"):
+	elif(arg[2] == "del"):
 		if(arg[3]):
 			 try:
 			 	todoarray.pop(arg[3]-1)
@@ -28,7 +28,7 @@ def todo(arg):
 			 except:
 			 	print("Error: todo #{} does not exist. Nothing deleted.".format(arg[3]))
     
-    else if(arg[2] == "done"):
+    elif(arg[2] == "done"):
 		if(arg[3]):
 		 	try:
 		 		done = todoarray.pop(arg[3]-1)
@@ -37,17 +37,17 @@ def todo(arg):
 		 	except:
 			 	print("Error: todo #{} does not exist.".format(arg[3]))
 	
-	else if(arg[2] == "report"):
+	elif(arg[2] == "report"):
 		today = date.today()
 		today = today.strftime("%Y-%m-%d")
 		print("{} Pending : {} Completed : {}".format(today, len(todoarray), len(donearray)))
 	
 	with open('todo.txt', 'w') as todofile:
-	       for item in todoarray:
-	       	todofile.write("%s\n" % item)
+	  for item in todoarray:
+	    todofile.write("%s\n" % item)
 	       	
 	with open('done.txt', 'w') as donefile:
-	       for item in todoarray:
-	       	donefile.write("%s\n" % item)
+	  for item in todoarray:
+	    donefile.write("%s\n" % item)
 		
 todo(sys.argv)
