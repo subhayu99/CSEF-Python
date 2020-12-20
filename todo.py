8dee4659e7e9594a7f39d04f0afafd40d43393e3
Subhayu Kumar Bala <balasubhayu99@gmail.com>
2020-12-20 18:29:25 +0530

    7th attempt

--- todo.py
+++ todo.py
@@ -3,55 +3,80 @@ import sys
 from datetime import date
 
 
-def todo(arg):
-    with open('todo.txt', 'w+') as todofile:
-        todoarray = list(todofile.readlines.split("\n"))
-    with open('done.txt', 'w+') as donefile:
-        donearray = list(donefile.readlines.split("\n"))
+def readtodo():
+    with open('todo.txt', 'r+') as todofile:
+         todoarray= todofile.readlines()
+    return todoarray
 
-    if (arg[2] == "help" or arg[2] == ""):
-        print('Usage :-\n$ ./todo add "todo item"  # Add a new todo\n$ ./todo ls               # Show remaining todos\n$ ./todo del NUMBER       # Delete a todo\n$ ./todo done NUMBER      # Complete a todo\n$ ./todo help             # Show usage\n$ ./todo report           # Statistics')
+def readdone():
+    with open('done.txt', 'r+') as donefile:
+         todoarray= donefile.readlines()
+    return todoarray
 
-    elif (arg[2] == "ls"):
-        for task, index in reversed(list(enumerate(todoarray))):
-            print("[{}] {}".format(index+1, task))
-
-    elif (arg[2] == "add"):
-        if(arg[3]):
-            todoarray.append(arg[3])
-            print("Added todo: {}".format(arg[3]))
-
-    elif(arg[2] == "del"):
-        if(arg[3]):
-            try:
-                todoarray.pop(arg[3]-1)
-                print("Deleted todo #{}".format(arg[3]))
-            except:
-                print("Error: todo #{} does not exist. Nothing deleted.".format(arg[3]))
-
-    elif(arg[2] == "done"):
-        if(arg[3]):
-            try:
-                today = date.today()
-                today = today.strftime("%Y-%m-%d")
-                done = todoarray.pop(arg[3]-1)
-                donearray.append("x " + today + " " + done)
-                print("Marked todo #{} as done.".format(arg[3]))
-            except:
-                print("Error: todo #{} does not exist.".format(arg[3]))
-
-    elif(arg[2] == "report"):
-        today = date.today()
-        today = today.strftime("%Y-%m-%d")
-        print("{} Pending : {} Completed : {}".format(today, len(todoarray), len(donearray)))
-
-    with open('todo.txt', 'w') as todofile:
+def savetodo(todoarray):
+    with open('todo.txt', 'a+') as todofile:
         for item in todoarray:
             todofile.write("%s\n" % item)
+    return 0
 
-    with open('done.txt', 'w') as donefile:
+def writetodo(todoarray):
+    with open('todo.txt', 'w+') as todofile:
         for item in todoarray:
-            donefile.write("%s\n" % item)
+            todofile.write("%s" % item)
+    return 0
+
+def writedone(donearray):
+    with open('done.txt', 'w+') as donefile:
+        for item in donearray:
+            donefile.write("%s" % item)
+    return 0
+
+
+def todo(arg):
+    if (len(arg) == 1 or arg[1] == "help"):
+        print('Usage :-\n$ ./todo add "todo item"  # Add a new todo\n$ ./todo ls               # Show remaining todos\n$ ./todo del NUMBER       # Delete a todo\n$ ./todo done NUMBER      # Complete a todo\n$ ./todo help             # Show usage\n$ ./todo report           # Statistics')
+
+    elif (arg[1] == "ls"):
+        todoarray = readtodo()
+        for index in range(len(todoarray)):
+            print("[" + str(len(todoarray) - index) + "] " + (todoarray[len(todoarray) - (index+1)].rstrip("\n")))
+
+    elif (arg[1] == "add"):
+        if(arg[2]):
+            todoarray = []
+            todoarray.append(arg[2])
+            savetodo(todoarray)
+            print("Added todo: {}".format(arg[2]))
+
+    elif(arg[1] == "del"):
+        if(arg[2]):
+            try:
+                todoarray = readtodo()
+                todoarray.pop(int(arg[2])-1)
+                print("Deleted todo #{}".format(arg[2]))
+                writetodo(todoarray)
+            except ValueError:
+                print("Error: todo #{} does not exist. Nothing deleted.".format(arg[2]))
+
+    elif(arg[1] == "done"):
+        if(arg[2]):
+            try:
+                todoarray = readtodo()
+                donearray = readdone()
+                today = (date.today()).strftime("%Y-%m-%d")
+                done = todoarray.pop(int(arg[2])-1)
+                donearray.append("x " + today + " " + done)
+                writetodo(todoarray)
+                writedone(donearray)
+                print("Marked todo #{} as done.".format(arg[2]))
+            except ValueError:
+                print("Error: todo #{} does not exist.".format(arg[2]))
+
+    elif(arg[1] == "report"):
+        today = (date.today()).strftime("%Y-%m-%d")
+        todoarray = readtodo()
+        donearray = readdone()
+        print("{} Pending : {} Completed : {}".format(today, len(todoarray), len(donearray)))
 
 
 todo(sys.argv)
