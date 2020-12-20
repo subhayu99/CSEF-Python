a7c26b3bd8bbe4eca9fa6b81ecfd271690afb1b1
Subhayu Kumar Bala <balasubhayu99@gmail.com>
2020-12-21 02:30:03 +0530

    Hoping the last try

--- todo.py
+++ todo.py
@@ -4,17 +4,25 @@ from datetime import date
 
 
 def readtodo():
-    with open('todo.txt', 'r+') as todofile:
-         todoarray= todofile.readlines()
-    return todoarray
+    try:
+        with open('todo.txt', 'r+') as todofile:
+             todoarray= todofile.readlines()
+        return todoarray
+    except FileNotFoundError:
+        with open('todo.txt', 'w+') as todofile:
+            todoarray= todofile.readlines()
+        return todoarray
+    except:
+    	print("There are no pending todos!")
 
 def readdone():
     try:
         with open('done.txt', 'r+') as donefile:
-             todoarray= donefile.readlines()
-        return todoarray
-    except:
-    	print("There are no pending todos!")
+             donearray= donefile.readlines()
+    except FileNotFoundError:
+        with open('done.txt', 'w+') as donefile:
+             donearray= donefile.readlines()
+    return donearray
 
 def savetodo(todoarray):
     with open('todo.txt', 'a+') as todofile:
@@ -23,16 +31,13 @@ def savetodo(todoarray):
     return 0
 
 def writetodo(todoarray):
-    try:
-        with open('todo.txt', 'w+') as todofile:
-            for item in todoarray:
-                todofile.write("%s" % item)
-    except:
-    	print("There are no pending todos!")
+    with open('todo.txt', 'w+') as todofile:
+        for item in todoarray:
+            todofile.write("%s" % item)
     return 0
 
-def writedone(donearray):
-    with open('done.txt', 'w+') as donefile:
+def savedone(donearray):
+    with open('done.txt', 'a') as donefile:
         for item in donearray:
             donefile.write("%s" % item)
     return 0
@@ -43,10 +48,7 @@ def todo(arg):
         print('Usage :-\n$ ./todo add "todo item"  # Add a new todo\n$ ./todo ls               # Show remaining todos\n$ ./todo del NUMBER       # Delete a todo\n$ ./todo done NUMBER      # Complete a todo\n$ ./todo help             # Show usage\n$ ./todo report           # Statistics')
 
     elif (arg[1] == "ls"):
-        try:
-            todoarray = readtodo()
-        except:
-        	print("There are no pending todos!")
+        todoarray = readtodo()
         for index in range(len(todoarray)):
             print("[" + str(len(todoarray) - index) + "] " + (todoarray[len(todoarray) - (index+1)].rstrip("\n")))
 
@@ -62,12 +64,12 @@ def todo(arg):
     elif(arg[1] == "del"):
         try:
             try:
-                if(arg[2] >= 0):
+                if(int(arg[2]) >= 0):
                     todoarray = readtodo()
                     todoarray.pop(int(arg[2])-1)
                     print("Deleted todo #{}".format(arg[2]))
                     writetodo(todoarray)
-                else:
+                elif(int(arg[2]) == 0):
                 	print("Error: todo #0 does not exist. Nothing to delete.")
             except:
                 print("Error: todo #{} does not exist. Nothing deleted.".format(arg[2]))
@@ -75,19 +77,19 @@ def todo(arg):
         	print("Error: Missing NUMBER for deleting todo.")
 
     elif(arg[1] == "done"):
-        try:
-            if(arg[2]):
+        if(len(arg) == 3):
+            try:
                 todoarray = readtodo()
                 donearray = readdone()
                 today = (date.today()).strftime("%Y-%m-%d")
                 done = todoarray.pop(int(arg[2])-1)
                 donearray.append("x " + today + " " + done)
                 writetodo(todoarray)
-                writedone(donearray)
+                savedone(donearray)
                 print("Marked todo #{} as done.".format(arg[2]))
-            else:
+            except:
                 print("Error: todo #{} does not exist.".format(arg[2]))
-        except:
+        elif(len(arg) < 3):
         	print("Error: Missing NUMBER for marking todo as done.")
 
     elif(arg[1] == "report"):
